ad0509427e5b8c51f47f1a665855ef31b3f6f136
Rami Halabi <ramiihalabi@outlook.com>
2022-07-29 23:44:05 -0500

    Create Project and Page Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">Logo</div>
+      <div className="header">Header</div>
+      <div className="nav">Nav</div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+
     </div>
   );
 }
