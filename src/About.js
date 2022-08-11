fa007fd8b5a5fdf38bfc150322c6e2dee05097f7
Rami Halabi <ramiihalabi@outlook.com>
2022-08-11 03:23:06 -0500

    About Section Work + Scroll Bar Styling Change

--- src/About.js
+++ src/About.js
@@ -8,9 +8,24 @@ const About = () =>{
         <>
             <div className="aboutSection">
                 <h2>Hello and Welcome!</h2>
-                <body>
-                    I am a senior at The University Of Oklahoma studying to by a Software Developer.
-                </body>
+
+                <div className="styling">
+                I am a senior at The University Of Oklahoma studying to be a Software Developer.
+                In my free time I like to stay active and I love learning about the latest technologies.
+                </div>
+            
+                <div className="styling">
+                As an Entry-Level Developer, I am eager to learn and solve problems to make meaningful contributions to my work. 
+                I have been described as communicative, adaptable, and regularly maintain an affable demeanor.                     
+                </div>
+
+                <div className="styling">
+                I am more than happy to share more information or meet with you! 
+                </div>
+                
+                <div className="styling">
+                EmailButton LinkedInButton
+                </div>
             </div>
     
             <img 
